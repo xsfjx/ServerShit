50438537412d2b05c2683b8da2788afb8d2cfca6
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-09-28 00:10:07 +0330

    Implement basic usage of sharedPref

--- app/src/main/java/com/servershit/MainActivity.java
+++ app/src/main/java/com/servershit/MainActivity.java
@@ -2,6 +2,11 @@ package com.servershit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etName = findViewById(R.id.etName);
+        final EditText etAge = findViewById(R.id.etAge);
+        final Switch theQ = findViewById(R.id.theQ);
+        Button setBtn = findViewById(R.id.setBtn);
+        Button getBtn = findViewById(R.id.getBtn);
+
+
+        setBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPref.setFName(etName.getText().toString());
+                SharedPref.setAge(Integer.parseInt(etAge.getText().toString()));
+                SharedPref.setSingle(theQ.isChecked());
+            }
+        });
+
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "F name is : " + SharedPref.getFName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Age is : " + SharedPref.getAge(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Single? " + SharedPref.isSingle(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }

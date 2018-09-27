c4a5bcbe474fed03bbc6fb247678cf50a7c8871f
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-09-28 00:02:45 +0330

    Define application class

---
+++ app/src/main/java/com/servershit/MyApplication.java
@@ -0,0 +1,16 @@
+package com.servershit;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+
+    static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+}

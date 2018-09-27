7b31a05caec61a935e4be778747b2d6532f1e296
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-09-28 00:02:04 +0330

    Add tags

---
+++ app/src/main/java/com/servershit/Tags.java
@@ -0,0 +1,15 @@
+package com.servershit;
+
+public class Tags {
+
+    /****************************   KEY   *************************/
+    public static final String F_NAME = "firstName";
+    public static final String AGE = "age";
+    public static final String SINGLE = "single";
+
+    /****************************   VALUE   *************************/
+
+    public static final String F_NAME_VALUE = "Saeed";
+    public static final int AGE_VALUE = 18;
+    public static final boolean SINGLE_VALUE = false;
+}

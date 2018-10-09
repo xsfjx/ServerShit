214007227aa6fc0bb99fbd2099770afea4f214a6
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-09 08:54:23 +0330

    Add broadcast receiver

---
+++ app/src/main/java/com/servershit/utils/InternetReceiver.java
@@ -0,0 +1,22 @@
+package com.servershit.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class InternetReceiver extends BroadcastReceiver
+
+    {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+        // an Intent broadcast.
+        try {
+            Toast.makeText(context, "onReceive: Oh shit", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
